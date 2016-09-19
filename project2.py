20ad624faff5d1e0d10ad658206970cf5c72de1a
Tomas Hernandez <tohernandez@csumb.edu>
2016-09-19 09:13:06 -0700

    Init commit

---
+++ project2.py
@@ -0,0 +1,8 @@
+#
+# * Title: project2.py
+# * Abstract:
+# * Authors: Andrew Diesh, Kieran Burke, Tomas Hernandez
+# * Github url: https://github.com/ttoti/CST205-Project2
+
+#!/usr/bin/env python3
+print("Hello World")
