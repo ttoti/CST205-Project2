02cbbf496b36f77f84fe3eb8fe1d2fd669f9aa40
Tomas Hernandez <tohernandez@csumb.edu>
2016-09-21 11:16:46 -0700

    Template code, starts webcam, grayscales and blurs

--- project2.py
+++ project2.py
@@ -5,4 +5,24 @@
 # * Github url: https://github.com/ttoti/CST205-Project2
 
 #!/usr/bin/env python3
-print("Hello World")
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+		# Our operations on the frame come here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(gray,(15,15),0)
+
+			# Display the resulting frame
+	cv2.imshow('frame',blur)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
