e3bfd0fa2443b53b9ac4b1801aa3225fc84f058e
Tomas Hernandez <tohernandez@csumb.edu>
2016-10-13 18:56:14 -0700

    Fixed getContours method and cleaned code

--- project2.py
+++ project2.py
@@ -4,6 +4,7 @@
 # *				to determine what shape 2 players pick
 # * Authors: Andrew Diesh, Kieran Burke, Tomas Hernandez
 # * Github url: https://github.com/ttoti/CST205-Project2
+# * Work file for Tomas Hernandez
 
 #!/usr/bin/env python3
 import numpy as np
@@ -11,56 +12,61 @@ import cv2
 
 #Defines camera to use
 cap = cv2.VideoCapture(0)
-#Creates BackgroundSubtractor
-fgbg = cv2.createBackgroundSubtractorMOG2(history = 2500)
-#Workaround for OpenCV error
-cv2.ocl.setUseOpenCL(False)
+
+#Creates BackgroundSubtractor #Uncomment for to apply mask
+#fgbg = cv2.createBackgroundSubtractorMOG2(history =5000)
+
+#Workaround for OpenCV error #Uncomment for to apply mask
+#cv2.ocl.setUseOpenCL(False)
 
 def mainProgram():
 	while(True):
 		# Capture frame-by-frame
 		ret, frame = cap.read()
 		if frame is not None:
-		# Operations to add effects for BackgroundSubtractor
-			cv2.rectangle(frame,(400,400),(100,100),(0,255,0),0)
-			cv2.rectangle(frame,(1100,400),(800,100),(0,255,0),0)
 
-			##Crop image to get contours of each
+			#Renders rectangles to show where to place hands
+			cv2.rectangle(frame,(600,600),(100,100),(0,255,0),0)
+			cv2.rectangle(frame,(1200,600),(700,100),(0,255,0),0)
+
+			#Applys filters to getContours
 			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-			blur = cv2.GaussianBlur(gray,(15,15),0)
-			ret, threshold = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-			fgmask = fgbg.apply(threshold)
-			#threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+			blur = cv2.GaussianBlur(gray,(11,11),0)
+			ret, threshold = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-			#Apply mask
+			#Apply mask #Uncomment for to apply mask
 			#fgmask = fgbg.apply(threshold)
-			crop_img2 = fgmask[100:400, 100:400]
-			crop_img = fgmask[100:400, 800:1100]
-			#Find contours then draws
-			#frame2, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-			#cv2.drawContours(threshold, contours, -1, (255,255,255), 3)
 
-			# Display the resulting frame
-			#cv2.imshow('Foreground',threshold)
+			#Crops the image
+			crop_img2 = threshold[100:600, 100:600]
+			crop_img = threshold[100:600, 700:1200]
 
-			#getContours(crop_img)
-			#getContours(crop_img2)
-			cv2.imshow('frame', getContours(crop_img))
-			#cv2.imshow('frame2', getContours(crop_img2))
-			cv2.imshow('main', frame)
+			firstImageArray = getContours(crop_img, frame)
+			secondImageArray = getContours(crop_img2, frame)
+
+			#Shows cropped image as a frame
+			cv2.imshow('crop_img', firstImageArray[0])
+			cv2.imshow('crop_img2',secondImageArray[0])
+
+			cv2.imshow('frame',frame)
+			#cv2.imshow('thresh', threshold)
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 
 	# When everything done, release the capture
 	cap.release()
+
 	cv2.destroyAllWindows()
 
-def getContours(cropped_image):
-	#This function currently doesn't work properly
+#Runs in main program, returns an array in order of cropped_image,count of contours, and frame
+def getContours(cropped_image, frame):
+
+	payload = []
 	frame2, contours, hierarchy = cv2.findContours(cropped_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	max_area =0
-	ci = 1
-	if(len(contours) >= 0):
+	max_area = 0
+	cnt = 0
+
+	try:
 		#hull(redline)
 		for i in range(len(contours)):
 			cnt = contours[i]
@@ -69,24 +75,33 @@ def getContours(cropped_image):
 				max_area = area
 				ci=i
 
-	cnt = contours[ci]
-	hull =cv2.convexHull(cnt)
-	frame2 =np.zeros(cropped_image.shape,np.uint8)
-	cv2.drawContours(cropped_image,[cnt],0,(0,255,0),1)
-	cv2.drawContours(cropped_image,[hull],0,(0,0,255),1)
+		cnt = contours[ci]
+		hull =cv2.convexHull(cnt)
+		frame2 =np.zeros(frame.shape,np.uint8)
+		cv2.drawContours(frame2,[cnt],0,(0,255,0),1)
+		cv2.drawContours(frame2,[hull],0,(0,0,255),1)
 
-	hull=cv2.convexHull(cnt,returnPoints = False)
-	defects = cv2.convexityDefects(cnt,hull)
+		hull=cv2.convexHull(cnt,returnPoints = False)
+		defects = cv2.convexityDefects(cnt,hull)
 
-	i = 0
-	for i in range(defects.shape[0]):
-		s,e,f,d = defects[i,0]
-		start = tuple(cnt[s][0])
-		end = tuple(cnt[e][0])
-		far = tuple(cnt[f][0])
-		cv2.line(cropped_image,start,end,[0,255,0],0)
-		cv2.circle(cropped_image,far,5,[0,0,255],0)
-		print(i)
-	return cropped_image
+		min = 0
+		max = 0
+		i = 0
+		for i in range(defects.shape[0]):
+			s,e,f,d = defects[i,0]
+			start = tuple(cnt[s][0])
+			end = tuple(cnt[e][0])
+			far = tuple(cnt[f][0])
+			cv2.line(frame,start,end,[0,255,0],2)
+			cv2.circle(frame,far,5,[0,0,255],-1)
+			#print(i)
 
+	except:
+		print("oops")
+
+	payload.append(cropped_image)
+	payload.append(cnt)
+	payload.append(frame)
+
+	return payload
 mainProgram()
