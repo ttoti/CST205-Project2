b78e94ee327cc63f7aca4a921c0174d43614a1ad
GnarGnar <kburke@csumb.edu>
2016-10-13 17:39:21 -0700

    Fixed it not starting with a try loop. Rectangles need to be adjusted.

--- Camera.py
+++ Camera.py
@@ -11,7 +11,7 @@ import cv2
 #Defines camera to use
 cap = cv2.VideoCapture(0)
 #Creates BackgroundSubtractor
-fgbg = cv2.createBackgroundSubtractorMOG2(history =2500)
+fgbg = cv2.createBackgroundSubtractorMOG2(history =5000)
 
 #Workaround for OpenCV error
 cv2.ocl.setUseOpenCL(False)
@@ -27,43 +27,52 @@ while(True):
 		ret, threshold = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 		
 		fgmask = fgbg.apply(threshold)
-		frame2, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		max_area =0
-	if(len(contours) >= 0):								
+		crop_img = fgmask[100:400, 100:400]
+		crop_img2 = fgmask[100:400, 800:1100]
+		
+		cv2.rectangle(frame,(50,50),(400,400),(0,255,0),0)
+		cv2.rectangle(frame,(1100,400),(800,100),(0,0,255),0)
+		frame2, contours, hierarchy = cv2.findContours(crop_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		max_area = 0		
+		
+		try:
+		
+			
 			#hull(redline)
-		for i in range(len(contours)):
-			cnt = contours[i]
-			area = cv2.contourArea(cnt)
-			if(area > max_area):
-				max_area = area
-				ci=i
+			for i in range(len(contours)):
+				cnt = contours[i]
+				area = cv2.contourArea(cnt)
+				if(area > max_area):
+					max_area = area
+					ci=i
 		
-		cnt = contours[ci]
-		hull =cv2.convexHull(cnt)
-		frame2 =np.zeros(frame.shape,np.uint8)
-		cv2.drawContours(frame2,[cnt],0,(0,255,0),1)
-		cv2.drawContours(frame2,[hull],0,(0,0,255),1)	
+			cnt = contours[ci]
+			hull =cv2.convexHull(cnt)
+			frame2 =np.zeros(frame.shape,np.uint8)
+			cv2.drawContours(frame2,[cnt],0,(0,255,0),1)
+			cv2.drawContours(frame2,[hull],0,(0,0,255),1)	
 		
-		hull=cv2.convexHull(cnt,returnPoints = False)
-		defects = cv2.convexityDefects(cnt,hull)
+			hull=cv2.convexHull(cnt,returnPoints = False)
+			defects = cv2.convexityDefects(cnt,hull)
 		
-		min =0
-		max =0
-		i =0
-		for i in range(defects.shape[0]):
-			s,e,f,d = defects[i,0]
-			start = tuple(cnt[s][0])
-			end = tuple(cnt[e][0])
-			far = tuple(cnt[f][0])
-			cv2.line(frame,start,end,[0,255,0],0)                
-			cv2.circle(frame,far,5,[0,0,255],0)
-			print(i)
+			min = 0
+			max = 0
+			i = 0
+			for i in range(defects.shape[0]):
+				s,e,f,d = defects[i,0]
+				start = tuple(cnt[s][0])
+				end = tuple(cnt[e][0])
+				far = tuple(cnt[f][0])
+				cv2.line(frame,start,end,[0,255,0],2)                
+				cv2.circle(frame,far,5,[0,0,255],-1)
+				print(i)
 		
-		
-			# Display the resulting frame
-		
+		except:
+			print("oops")
+	# Display the resulting frame
+			
 	cv2.imshow('mask',fgmask)
-	cv2.imshow('frame',frame)
+	cv2.imshow('frame',frame2)
 	cv2.imshow('thresh', threshold)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
